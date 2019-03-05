96c6ab3f7480e64e2cdb1da672a732c0c5c9fb04
Staffan <staffan.sandberg94@gmail.com>
2019-03-05 22:00:46 +0100

    Typo fix

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if(process.env.NODE_ENV === "production"){
 }
 
 app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'build', 'index.html'));
+	response.sendFile(path.join(__dirname, 'build/', 'index.html'));
 });
 
 // Client connects
