1ac74604695da63e479c2beb1ed23c96cdec1e96
Staffan Sandberg <staffansandberg@outlook.com>
2020-12-12 18:42:00 +0100

    Port update

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,56 +1,55 @@
-import openSocket from 'socket.io-client'
+import openSocket from "socket.io-client";
 var socket;
-if(process.env.NODE_ENV === 'production')
-  socket = openSocket();
-else
-  socket = openSocket('http://localhost:3001');
+var PORT = process.env.PORT;
+if (process.env.NODE_ENV === "production") socket = openSocket();
+else socket = openSocket(`http://localhost:${PORT}`);
 
 export const subscribeToTimer = (interval, cb) => {
-  socket.on('timer', timestamp => cb(null, timestamp));
-  socket.emit('subscribeToTimer', interval);
+  socket.on("timer", (timestamp) => cb(null, timestamp));
+  socket.emit("subscribeToTimer", interval);
 };
 
 // Listen for all mouse positions
-export const subscribeToMousePositions = cb => {
-  socket.on('newMousePositions', mousePositions => cb(mousePositions));
-}
+export const subscribeToMousePositions = (cb) => {
+  socket.on("newMousePositions", (mousePositions) => cb(mousePositions));
+};
 
-export const subscribeToShotsFired = cb => {
-  socket.on('shotsFired', positions => cb(positions));
-}
+export const subscribeToShotsFired = (cb) => {
+  socket.on("shotsFired", (positions) => cb(positions));
+};
 
-export const subscribeToScoreboard = cb => {
-  socket.on('scoreboard', scoreboard => cb(scoreboard));
-}
+export const subscribeToScoreboard = (cb) => {
+  socket.on("scoreboard", (scoreboard) => cb(scoreboard));
+};
 
-export const subscribeToNewTarget = cb => {
-  socket.on('newTarget', target => cb(target));
-}
+export const subscribeToNewTarget = (cb) => {
+  socket.on("newTarget", (target) => cb(target));
+};
 
-export const subscribeToWinner = cb => {
-  socket.on('winner', winner => cb(winner));
-}
+export const subscribeToWinner = (cb) => {
+  socket.on("winner", (winner) => cb(winner));
+};
 
-export const subscribeToWaitForNextRound = cb => {
-  socket.on('waitingForNextRound', timer => cb(timer));
-}
+export const subscribeToWaitForNextRound = (cb) => {
+  socket.on("waitingForNextRound", (timer) => cb(timer));
+};
 
-export const subscribeToMyID = cb => {
-  socket.on('myID', myID => cb(myID));
-}
+export const subscribeToMyID = (cb) => {
+  socket.on("myID", (myID) => cb(myID));
+};
 
 // Emit mouse position 60 times per second here!
-export const sendMousePosition = mousePosition => {
-  socket.emit('mousePosition', mousePosition);
-}
+export const sendMousePosition = (mousePosition) => {
+  socket.emit("mousePosition", mousePosition);
+};
 
-export const sendMouseClickPosition = mouseClickPosition => {
-  socket.emit('mouseClickPosition', mouseClickPosition);
-}
+export const sendMouseClickPosition = (mouseClickPosition) => {
+  socket.emit("mouseClickPosition", mouseClickPosition);
+};
 
-export const sendNicknameToServer = nickname => {
-  socket.emit('nickname', nickname);
-}
+export const sendNicknameToServer = (nickname) => {
+  socket.emit("nickname", nickname);
+};
 
 export const unsubscribeFromAll = () => {
   socket.removeAllListeners("newMousePositions");
@@ -58,4 +57,4 @@ export const unsubscribeFromAll = () => {
   socket.removeAllListeners("scoreboard");
   socket.removeAllListeners("newTarget");
   socket.removeAllListeners("myID");
-}
+};
